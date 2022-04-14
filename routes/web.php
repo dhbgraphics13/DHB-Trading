4e45222e9361ef006fdb47b74a0cb5f9924c4f45
DHB GRAPHICS <58020706+dhbgraphics13@users.noreply.github.com>
2022-04-14 15:54:19 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -26,11 +26,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-/*Route::get('/', function () {
-    return view('welcome');
-});*/
+Route::get('/', function () {
+    return redirect()->route('home');
+    //return view('welcome');
+});
 
-Route::get('/', [FrontController::class, 'index'])->name('welcome');
+
+//Route::get('/', [FrontController::class, 'index'])->name('welcome');
 
 Route::group([ 'middleware' =>  ['auth' ]], function() { //all users
     Route::get('/home', [HomeController::class, 'index'])->name('home');
