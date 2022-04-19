84bedf58ccc59708af377fecc493b763b2a698fa
DHB GRAPHICS <58020706+dhbgraphics13@users.noreply.github.com>
2022-04-19 12:10:39 +0530

    Update 2022_03_23_053432_create_orders_table.php

--- database/migrations/2022_03_23_053432_create_orders_table.php
+++ database/migrations/2022_03_23_053432_create_orders_table.php
@@ -16,11 +16,11 @@ return new class extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->bigInteger('user_id');
-            $table->string('name');
+            $table->string('name')->nullable();
             $table->string('phone')->nullable();
             $table->string('email')->nullable();
             $table->string('address')->nullable();
-            $table->mediumText('order_details');
+            $table->mediumText('order_details')->nullable();
             $table->enum('status',['0','1','2','3'])->default('0')->comment('0=>pending,1=>process,2=>done,3=>cancel');
             $table->date('due_date')->nullable()->comment('not necessary its work up down');
             $table->string('payment_method')->nullable();
@@ -28,6 +28,7 @@ return new class extends Migration
             $table->string('uuid');
             $table->date('job_done_on')->nullable();
             $table->timestamps();
+             $table->date('deleted_at')->nullable();
         });
     }
 
